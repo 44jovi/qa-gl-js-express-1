05c14b4a9d13b2a8623dfa0a718747dd0eca3644
44jovi <kwong.jovi@gmail.com>
2023-04-25 10:21:03 +0100

    progress - GET, POST

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
-const server = app.listen(4494, () => {
-  console.log("server started on port", server.address().port);
+// Example of middlewware
+// Parses body of requests and responses to JSON
+app.use(express.json());
+
+const cats = [];
+
+app.get("/getAll", (req, res) => {
+  // Alternative to res.send() but also converts non-objects to JSON
+  res.json(cats);
+});
+
+app.post("/create", (req, res) => {
+  const newCat = req.body;
+  cats.push(newCat);
+  res.status(201).json(cats[cats.length - 1]);
 });
 
 app.get("/hello", (req, res) => {
   res.send("Howdy, world!");
+});
+
+const server = app.listen(4494, () => {
+  console.log("server started on port", server.address().port);
 });
