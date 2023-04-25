30b9829701506f2ca8344ead3e819a04d991a56e
44jovi <kwong.jovi@gmail.com>
2023-04-25 09:53:44 +0100

    progress

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const express = require("express");
+const app = express();
+const server = app.listen(4494, () => {
+  console.log("server started on port", server.address().port);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Howdy, world!");
+});
