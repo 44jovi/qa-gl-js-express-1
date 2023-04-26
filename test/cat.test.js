a94958141aadd8a964d45217573bc1c3b5a0f4a1
44jovi <kwong.jovi@gmail.com>
2023-04-26 12:40:28 +0100

    progress(cat-test.js) - beforeEach to create a cat

--- test/cat.test.js
+++ test/cat.test.js
@@ -4,15 +4,27 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 
 const server = require("../index");
+const { catModel } = require("../db");
 
 const mongoose = require("mongoose");
 
-before(() => {
-  mongoose.connection.db.dropDatabase();
+// before(() => {
+//   mongoose.connection.db.dropDatabase();
+// });
+
+beforeEach(async () => {
+  await catModel.deleteMany({});
+  testCat = await catModel.create({
+    name: "test cat!",
+    colour: "orange",
+    evil: true,
+  });
+  testCat = JSON.stringify(testCat);
+  testCat = JSON.parse(testCat);
 });
 
 describe("API tests", () => {
-  it("should create a cat", (done) => {
+  it("/create should create a cat", (done) => {
     const cat = { name: "whiskerz", colour: "blue", evil: true };
 
     chai
@@ -28,17 +40,17 @@ describe("API tests", () => {
       });
   });
 
-  it("should get all cats", (done) => {
+  it("/getAll should get all cats", (done) => {
     chai
       .request(server)
       .get("/cats/getAll")
       .send()
       .end((err, res) => {
         chai.expect(err).to.be.null;
-        console.log(res.body);
-        // Point to last cat created
-        chai.expect(res.body[res.body.length - 1].name).to.equal("whiskerz");
-        // TODO: check cat id
+        console.log(res.body[0]);
+        chai.expect(res.body[0]._id).to.equal(testCat._id);
+        // Confirm it was the last cat created
+        chai.expect(res.body[res.body.length - 1].name).to.equal("test cat!");
         chai.expect(res.status).to.equal(200);
         done();
       });
