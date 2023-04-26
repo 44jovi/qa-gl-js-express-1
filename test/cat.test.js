de0c0b75a783555dc6b66854bfd142b5e829a1b6
44jovi <kwong.jovi@gmail.com>
2023-04-26 12:04:34 +0100

    add  block

--- test/cat.test.js
+++ test/cat.test.js
@@ -5,9 +5,15 @@ chai.use(chaiHttp);
 
 const server = require("../index");
 
+const mongoose = require("mongoose");
+
+before(() => {
+  mongoose.connection.db.dropDatabase();
+});
+
 describe("API tests", () => {
   it("should create a cat", (done) => {
-    const cat = { name: "blah", colour: "blue", evil: true };
+    const cat = { name: "whiskerz", colour: "blue", evil: true };
 
     chai
       .request(server)
@@ -20,5 +26,25 @@ describe("API tests", () => {
         // End of test
         done();
       });
+  });
+
+  it("should get all cats", (done) => {
+    chai
+      .request(server)
+      .get("/cats/getAll")
+      .send()
+      .end((err, res) => {
+        chai.expect(err).to.be.null;
+        console.log(res.body);
+        // Point to last cat created
+        chai.expect(res.body[res.body.length - 1].name).to.equal("whiskerz");
+        // TODO: check cat id
+        chai.expect(res.status).to.equal(200);
+        done();
+      });
+  });
+
+  after(async () => {
+    await mongoose.disconnect();
   });
 });
