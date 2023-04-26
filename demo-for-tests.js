7aa23375b8e7e61cdde8b93d06ec9e4e90fe64c3
44jovi <kwong.jovi@gmail.com>
2023-04-26 10:14:22 +0100

    pass tests for isPrimeNum()

---
+++ demo-for-tests.js
@@ -0,0 +1,15 @@
+function isPrimeNum(n) {
+  if (n == 1) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = { isPrimeNum };
